4ad222f0024e2408339073f47435eec7c90e4080
Henry Budden <henrybudden@hotmail.co.uk>
2016-09-01 17:23:32 +0100

    Update 01_sunrise_alarm.py

--- 01_sunrise_alarm.py
+++ 01_sunrise_alarm.py
@@ -6,37 +6,28 @@ import time, math
 
 GPIO.cleanup()
 
-# Configure the Pi to use the BCM (Broadcom) pin names, rather than the pin positions
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM) # Configure the Pi to use the BCM (Broadcom) pin names, rather than the pin positions
 
+sunrise = 50 # Set sunrise limit. This works for testing in a light room when torch aimed at LDR. Use 30 for accurate sunrise detection.
 
-# Set sunrise limit. This works for testing in a light room when torch aimed at LDR. Use 30 for accurate sunrise detection.
-sunrise = 50
+a_pin = 18 # Pin a charges the capacitor through a fixed 1k resistor and the thermistor in series
+b_pin = 23 # pin b discharges the capacitor through a fixed 1k resistor 
 
-# Pin a charges the capacitor through a fixed 1k resistor and the thermistor in series
-# pin b discharges the capacitor through a fixed 1k resistor 
-a_pin = 18
-b_pin = 23
-
-# Setup pins as outputs for the buzzer and the two LEDs
-buzzer_pin = 24
-red_pin1 = 27
+buzzer_pin = 24 # Setup buzzer pin as output
+red_pin1 = 27 # Setup led pins as outputs
 red_pin2 = 22
 
 GPIO.setup(buzzer_pin, GPIO.OUT)
 GPIO.setup(red_pin1, GPIO.OUT)
 GPIO.setup(red_pin2, GPIO.OUT)
 
-# empty the capacitor ready to start filling it up
-def discharge():
+def discharge(): # empty the capacitor ready to start filling it up
     GPIO.setup(a_pin, GPIO.IN)
     GPIO.setup(b_pin, GPIO.OUT)
     GPIO.output(b_pin, False)
     time.sleep(0.01)
 
-# return the time taken for the voltage on the capacitor to count as a digital input HIGH
-# than means around 1.65V
-def charge_time():
+def charge_time(): # return the time taken for the voltage on the capacitor to count as a digital input HIGH than means around 1.65V
     GPIO.setup(b_pin, GPIO.IN)
     GPIO.setup(a_pin, GPIO.OUT)
     GPIO.output(a_pin, True)
@@ -46,14 +37,11 @@ def charge_time():
     t2 = time.time()
     return (t2 - t1) * 1000000
 
-# Take an analog readin as the time taken to charge after first discharging the capacitor
-def analog_read():
+def analog_read(): # Take an analog reading as the time taken to charge after first discharging the capacitor
     discharge()
     return charge_time()
 
-# Convert the time taken to charge the cpacitor into a value of resistance
-# To reduce errors, do it 100 times and take the average.
-def read_resistance():
+def read_resistance(): # Convert the time taken to charge the cpacitor into a value of resistance to reduce errors, do it 100 times and take the average.
     n = 20
     total = 0;
     for i in range(1, n):
@@ -62,8 +50,7 @@ def read_resistance():
     resistance = reading * 6.05 - 939
     return resistance
 
-def light_from_r(R):
-    # Log the reading to compress the range
+def light_from_r(R):     # Log the reading to compress the range
     return math.log(1000000.0/R) * 10.0 
 
 while True:
